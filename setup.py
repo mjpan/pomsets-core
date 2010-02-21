d64f7cfa11eeaee3510b05484276e531acae7084
Michael Pan <mjpan@nephosity.com>
2010-02-21 19:51:07 +0000

    update setup.py to version 1.0.1
    git-svn-id: http://svn.pomsets.org/pomsets-core/trunk@39 afb4775a-5b19-4acc-a68a-306422b08c5a

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pomsets-core',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(
         'src',exclude=["*.test", "*.test.*", "test.*", "test",
                        "*.utils", "*.utils.*", "utils.*", "utils",]),
